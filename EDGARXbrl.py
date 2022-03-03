57ee9c110ae7f25edb27979717bf98703fb846b8
Parth Patel <44256871+PPP180000@users.noreply.github.com>
2022-03-03 15:40:15 -0800

    Update EDGARXbrl.py

--- EDGARXbrl.py
+++ EDGARXbrl.py
@@ -71,8 +71,8 @@ sns.set_style('whitegrid')
 
 data_path = st.write('Volumes/Seagate Backup Plus Drive/Parth Patel_Mac/SEC Data/') # perhaps set to external harddrive to accomodate large amount of data
 
-# if not data_path.exists():
-#     data_path.mkdir()
+if not data_path :
+    data_path.mkdir(exist_ok=True)
 
 
 # The following code downloads and extracts all historical filings contained in the Financial Statement and Notes (FSN) datasets for the given range of quarters:
