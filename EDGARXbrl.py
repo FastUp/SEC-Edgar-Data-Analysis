81ab99144411e28b929808317da7dc1efc5e8c42
Parth Patel <44256871+PPP180000@users.noreply.github.com>
2022-03-03 15:58:12 -0800

    Update EDGARXbrl.py

--- EDGARXbrl.py
+++ EDGARXbrl.py
@@ -103,7 +103,8 @@ filing_periods.extend([(this_year, q) for q in range(1, this_quarter + 1)])
 
 # In[8]:
 
-
+yr = y
+qtr = q
 
 for i, (yr, qtr) in enumerate(filing_periods, 1):
     print(f'{yr}-Q{qtr}', end=' ', flush=True)
